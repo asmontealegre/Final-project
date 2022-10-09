682494e09cc5d2d1276693de1b0f58dd03fad883
asmontealegre7 <asmontealegre7@misena.edu.co>
2022-10-09 16:45:46 -0500

    Actualizado01

--- General/urls.py
+++ General/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from General.views import holamundo
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",holamundo , name="inicio"),
 ]
