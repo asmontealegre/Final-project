a4b3c4316c08b9e7db7acada984bafc9daa033ca
asmontealegre7 <113529205+asmontealegre@users.noreply.github.com>
2022-10-26 00:31:35 -0500

    26/10

--- administracion/views.py
+++ administracion/views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def administracion(request):
+    titulo="Tablero Principal"
     context={
-        
+        'titulo': titulo
     }
     return render(request,'administracion/index-admin.html',context)
 
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from usuarios.views import usuarios
+
 urlpatterns = [
     path('',usuarios,name="usuarios"),
+    
 ]
